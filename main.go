3cd411073ddc7605d3ea771a591a7abf9f32480e
PedroHODL <pedrohenrique.lopes@mercadolivre.com>
2022-05-19 10:44:47 -0300

    primeiro programa Go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Olá Mundo!")
+
+	const hora string = "Pedro"
+	var boolean bool = true
+	var massa uint = 20
+	fmt.Println(boolean, massa)
+}
